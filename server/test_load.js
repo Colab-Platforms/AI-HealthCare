0c4e71c36fc875a395ab4624f8543cc8bbd6292d
Colab-Platforms <colabplatforms@gmail.com>
2026-04-14 15:17:21 +0530

    Delete server/test_load.js

--- server/test_load.js
+++
@@ -1,8 +0,0 @@
-
-const adminRoutes = require('./routes/adminRoutes');
-console.log('Admin routes loaded successfully');
-const adminController = require('./controllers/adminController');
-console.log('Admin controller loaded successfully');
-const doctorController = require('./controllers/doctorController');
-console.log('Doctor controller loaded successfully');
-process.exit(0);
